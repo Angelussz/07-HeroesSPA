84a479b030bb97a64a4275d1448920e198d94709
angelo <aperezr@unsa.edu.pe>
2024-03-19 22:12:45 -0500

    recordar ultima ruta

--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -1,8 +1,11 @@
 import React, { useContext } from 'react'
 import { AuthContext } from '../auth'
-import { Navigate } from 'react-router-dom'
+import { Navigate, useLocation} from 'react-router-dom'
 
 export const PrivateRoute = ({children}) => {
     const {logged} =useContext(AuthContext)
+    const {pathname,search} = useLocation();
+    const lastpath = pathname+search;
+    localStorage.setItem('lastPath',lastpath)
   return (logged)?children:<Navigate to={"/login"} />
 }
