c620c199d34cd3376a1c56a2e2616c465e95b1be
angelo <aperezr@unsa.edu.pe>
2024-03-19 19:21:30 -0500

    rutas privadas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { LoginPage } from '../auth'
 import {HeroRouter} from '../heroes'
+import { PrivateRoute } from './PrivateRoute'
 export const AppRouter = () => {
   return (
     <>
@@ -9,8 +10,11 @@ export const AppRouter = () => {
         <Route path='/login' element={<LoginPage />} />
         {/* <Route path='/' element={<HeroRouter />} /> */}
         {/* <Route path='/*' element={<div>Error pAge</div>} /> */}
-        <Route path='/*' element={<HeroRouter />} />
-        
+        <Route path='/*' element ={
+          <PrivateRoute>
+            <HeroRouter />
+          </PrivateRoute>
+        } />
     </Routes>
     </>
   )
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link, NavLink, useNavigate} from "react-router-dom";
+import { AuthContext } from "../../auth";
 const navActive = ({ isActive }) =>
   `nav-item nav-link ${isActive ? "active" : ""}`;
 export const Navbar = () => {
+  const {user,logout} = useContext(AuthContext)
   const navigate = useNavigate();
   const onLogOut = ()=>{
+    logout();
     navigate("/login",{
       replace:true
     })
@@ -36,7 +39,7 @@ export const Navbar = () => {
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
           <span className="nav-item nav-link text-primary">
-            Angelo
+            {user?.name}
           </span>
           <button className="nav-item nav-link btn" onClick={onLogOut}>
             Logout
