ec7b0fc7c63f5b1d0894c4bd8c102331002c4466
angelo <aperezr@unsa.edu.pe>
2024-03-19 19:49:36 -0500

    rutas publicas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,21 +1,32 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import { LoginPage } from '../auth'
-import {HeroRouter} from '../heroes'
-import { PrivateRoute } from './PrivateRoute'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { LoginPage } from "../auth";
+import { HeroRouter } from "../heroes";
+import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 export const AppRouter = () => {
   return (
     <>
-    <Routes>
-        <Route path='/login' element={<LoginPage />} />
+      <Routes>
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <LoginPage />
+            </PublicRoute>
+          }
+        />
         {/* <Route path='/' element={<HeroRouter />} /> */}
         {/* <Route path='/*' element={<div>Error pAge</div>} /> */}
-        <Route path='/*' element ={
-          <PrivateRoute>
-            <HeroRouter />
-          </PrivateRoute>
-        } />
-    </Routes>
+        <Route
+          path="/*"
+          element={
+            <PrivateRoute>
+              <HeroRouter />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
     </>
-  )
-}
+  );
+};
